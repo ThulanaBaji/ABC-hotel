415aaa7795fedf54ab45007b757e4e49d92bdf03
Thulana2006 <thulanabaji@gmail.com>
2024-02-04 21:07:42 +0530

    bill card modified

--- application/views/acc/dashboard.php
+++ application/views/acc/dashboard.php
@@ -18,7 +18,7 @@
                     <a href="<?= base_url('acc/dashboard/logout') ?>" class="p-2 w-24 text-center rounded text-sm font-semibold text-white bg-gray-600 hover:cursor-pointer">New bill</a>
                     <div class=" max-w-lg w-full">
                         <div class="flex max-w-xl w-full">
-                            <select class="bg-gray-50 border focus:ring focus:ring-blue-500 rounded-l-lg border-gray-300 outline-none z-10" id="filter-selection">
+                            <select class="bg-gray-50 text-gray-500 text-center border focus:ring focus:ring-blue-500 rounded-l-lg border-gray-300 outline-none z-10" id="filter-selection">
                                 <option value="1">reference</option>
                                 <option value="2">Customer name</option>
                                 <option value="3">Date</option>
@@ -36,19 +36,47 @@
                     </div>
                 </div>
 
-                <div class="bills w-full pt-8 px-4 flex flex-col justify-center">
-                    <div class="bill rounded-lg border p-4 bg-gray-100 max-w-xl flex">
+                <div class="bills w-full pt-8 px-4 flex flex-col items-center">
+                    <div class="bill group relative rounded-lg border p-4 w-full bg-gray-50 max-w-xl flex justify-between">
+                        <div class="absolute right-0 top-0 hidden group-hover:flex">
+                            <button class="rounded-l-lg p-2 bg-yellow-500">
+                                <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M5 7.8C6.7 6.3 9.2 5 12 5s5.3 1.3 7 2.8a12.7 12.7 0 0 1 2.7 3.2c.2.2.3.6.3 1s-.1.8-.3 1a2 2 0 0 1-.6 1 12.7 12.7 0 0 1-9.1 5c-2.8 0-5.3-1.3-7-2.8A12.7 12.7 0 0 1 2.3 13c-.2-.2-.3-.6-.3-1s.1-.8.3-1c.1-.4.3-.7.6-1 .5-.7 1.2-1.5 2.1-2.2Zm7 7.2a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd"/>
+                                </svg>
+                                
+                            </button>
+                            <button class="rounded-r-lg p-2 bg-red-600">
+                                <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M8.6 2.6A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4c0-.5.2-1 .6-1.4ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
+                                </svg>
+                                
+                            </button>
+                        </div>                
+
                         <div class="detailsfirst flex flex-col gap-2">
-                            <div class="flex gap-2 text-gray-700 text-sm">
+                            <div class="flex gap-2 text-gray-600 text-sm">
                                 <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                                     <path fill-rule="evenodd" d="M11.3 3.3a1 1 0 0 1 1.4 0l6 6 2 2a1 1 0 0 1-1.4 1.4l-.3-.3V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3c0 .6-.4 1-1 1H7a2 2 0 0 1-2-2v-6.6l-.3.3a1 1 0 0 1-1.4-1.4l2-2 6-6Z" clip-rule="evenodd"/>
-                                </svg> <p class="text-xs font-semibold "> deluxe room</p>
+                                </svg> <p class="text-sm font-semibold "> deluxe room</p>
                             </div>
-                            <div class="flex gap-2 text-gray-700 text-sm">
-                                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                    <path fill-rule="evenodd" d="M11.3 3.3a1 1 0 0 1 1.4 0l6 6 2 2a1 1 0 0 1-1.4 1.4l-.3-.3V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3c0 .6-.4 1-1 1H7a2 2 0 0 1-2-2v-6.6l-.3.3a1 1 0 0 1-1.4-1.4l2-2 6-6Z" clip-rule="evenodd"/>
-                                </svg> <p class="text-xs font-semibold "> deluxe room</p>
+                            <div class="flex gap-1 mt-2 text-gray-600 text-sm">
+                                <svg class="h-4 w-4 fill-gray-600" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <style type="text/css"> .st0{fill:#000000;} </style> <g> <path class="st0" d="M78.641,118.933c22.88,0,41.416-18.551,41.416-41.414c0-22.887-18.536-41.423-41.416-41.423 c-22.887,0-41.422,18.536-41.422,41.423C37.218,100.382,55.754,118.933,78.641,118.933z"></path> <path class="st0" d="M255.706,228.73v0.062c0.101,0,0.194-0.031,0.294-0.031c0.101,0,0.194,0.031,0.294,0.031v-0.062 c15.562-0.317,28.082-12.976,28.082-28.601c0-15.648-12.52-28.299-28.082-28.616v-0.063c-0.101,0-0.194,0.031-0.294,0.031 c-0.1,0-0.193-0.031-0.294-0.031v0.063c-15.563,0.317-28.082,12.968-28.082,28.616C227.623,215.754,240.143,228.413,255.706,228.73 z"></path> <path class="st0" d="M433.359,118.933c22.887,0,41.423-18.551,41.423-41.414c0-22.887-18.536-41.423-41.423-41.423 c-22.88,0-41.416,18.536-41.416,41.423C391.944,100.382,410.48,118.933,433.359,118.933z"></path> <path class="st0" d="M470.097,138.553h-36.312h-18.404c-21.106,0-40.432,11.831-50.033,30.622l-33.494,97.967 c-1.154,2.246-3.298,3.84-5.792,4.282c-2.493,0.442-5.048-0.309-6.914-2.036l-20.836-18.04c-6.233-5.769-14.408-8.973-22.902-8.973 H256h-19.41c-8.494,0-16.669,3.204-22.902,8.973l-20.835,18.04c-1.866,1.727-4.421,2.478-6.914,2.036 c-2.492-0.442-4.637-2.036-5.791-4.282l-33.495-97.967c-9.6-18.791-28.926-30.622-50.032-30.622H78.215H41.902 C21.834,138.553,0,160.387,0,180.464v139.211c0,10.034,8.13,18.171,18.164,18.171c4.939,0,0,0,12.682,0l6.906,118.725 c0,10.676,8.664,19.332,19.34,19.332c4.506,0,12.814,0,21.122,0c8.308,0,16.616,0,21.122,0c10.676,0,19.34-8.656,19.34-19.332 l6.906-118.725l-0.085-84.766c0-1.339,0.914-2.493,2.222-2.818c1.309-0.31,2.648,0.309,3.26,1.502l26.572,65.401 c3.206,6.256,9.152,10.654,16.074,11.885c6.922,1.231,14.022-0.844,19.186-5.613l25.426-18.729 c0.852-0.782,2.083-0.984,3.136-0.542c1.061,0.473,1.743,1.518,1.743,2.663l0.093,73.508l4.777,82.187 c0,7.387,6.001,13.379,13.395,13.379c3.113,0,8.865,0,14.618,0c5.753,0,11.506,0,14.618,0c7.394,0,13.394-5.992,13.394-13.379 l4.778-82.187l0.093-73.508c0-1.146,0.681-2.19,1.742-2.663c1.053-0.442,2.284-0.24,3.136,0.542l25.427,18.729 c5.164,4.769,12.264,6.844,19.186,5.613c6.922-1.231,12.868-5.629,16.073-11.885l26.573-65.401 c0.611-1.192,1.951-1.812,3.259-1.502c1.309,0.325,2.222,1.478,2.222,2.818l-0.085,84.766l6.906,118.725 c0,10.676,8.664,19.332,19.341,19.332c4.507,0,12.814,0,21.122,0c8.308,0,16.616,0,21.121,0c10.677,0,19.342-8.656,19.342-19.332 l6.906-118.725c12.682,0,7.742,0,12.682,0c10.034,0,18.164-8.137,18.164-18.171V180.464 C512,160.387,490.166,138.553,470.097,138.553z"></path> </g> </g></svg>
+                                <p class="text-xs ml-2 font-semibold ">2 <p class="rounded-full text-gray-500 text-xs font-semibold px-1 bg-gray-200">kids</p></p>
+                                <p class="text-xs font-semibold ">4 <p class="rounded-full text-gray-500 text-xs font-semibold px-1 bg-gray-200">adults</p></p>
                             </div>
+                            <div class="flex gap-2 mt-3 text-gray-600 text-sm items-center">
+                                <svg class="w-3 h-3 text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm14-7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm-5-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm-5-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4Z"></path>
+                                </svg>
+                                <div class="flex flex-col gap-1">
+                                    <span class="text-xs ml-2 font-semibold flex gap-2">12 Jan, 23 <p class="rounded-full text-gray-500 text-xs w-16 text-center font-semibold px-1 bg-gray-200">check-in</p></span>
+                                    <span class="text-xs ml-2 font-semibold flex gap-2">23 Jan, 23 <p class="rounded-full text-gray-500 text-xs w-16 text-center font-semibold px-1 bg-gray-200">check-out</p></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="detailsfirst flex flex-col justify-between">
+                            <span class="text-lg font-semibold text-gray-700 flex items-center pt-5 gap-1">E0934SIU <p class="rounded-full text-gray-500 text-xs  text-center font-semibold px-2 h-fit bg-gray-200">ref</p></span>
+                            <span class="text-lg font-semibold text-gray-700 flex items-center ">2,000.00<p class="rounded-full text-gray-500 text-sm text-center font-semibold pl-2 h-fit items-baseline">Rs</p> </span>
                         </div>
                     </div>
                 </div>
